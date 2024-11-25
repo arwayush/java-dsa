71a89c475291fb983a4bec1bcb64f613d40804c1
arwayush <purisanamjee@gmail.com>
2024-11-25 23:14:00 +0530

    commited to the first code of the PRO DSA

---
+++ step-3/step3/src/googleSDE3/MinNumberOfBulletsToKillAllPeole.java
@@ -0,0 +1,33 @@
+package googleSDE3;
+
+import java.util.Arrays;
+
+public class MinNumberOfBulletsToKillAllPeole {
+
+
+    private static int bulletsForAll(int[] arr, int m){
+        int totalBullets = 0;
+        for(int strength : arr){
+            totalBullets += Math.ceil((double) strength / m);
+        }
+        return totalBullets;
+    }
+
+    private  static  int bulletsForKperson(int[] people, int m, int k){
+        Arrays.sort(people);
+        int bullets = 0;
+        for (int i = 0; i < k; i++) {
+            bullets += Math.ceil((double)  people[i] / m);
+        }
+        return  bullets;
+    }
+
+
+    public static void main(String[] args) {
+        int[] people = {15, 10, 20};
+        int m = 5;
+        int k = 2;
+        System.out.println("Bullets to kill all people: " +bulletsForAll(people, m));
+        System.out.println("Bullets to kill at least: " + bulletsForKperson(people, m, k));
+    }
+}
